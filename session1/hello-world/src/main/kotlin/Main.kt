810c00f513e59d6ce03bb494366c1b07100cea22
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-10-05 11:45:03 +0200

    Hello World Dynamisch Af

--- session1/hello-world/src/main/kotlin/Main.kt
+++ session1/hello-world/src/main/kotlin/Main.kt
@@ -1,3 +1,9 @@
 fun main(){
     println("Hello World")
+
+    val classmates = arrayOf("Jens", "Sacha", "Jasmien", "Tyron", "David")
+
+    for (mates in classmates){
+        println ("Hello" + mates)
+    }
 }
