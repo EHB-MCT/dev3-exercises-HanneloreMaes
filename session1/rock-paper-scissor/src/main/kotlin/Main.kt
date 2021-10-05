25824b1f77cb4df45bb4579f1592562094dcdfec
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-10-05 12:58:45 +0200

    Begin win/lose

--- session1/rock-paper-scissor/src/main/kotlin/Main.kt
+++ session1/rock-paper-scissor/src/main/kotlin/Main.kt
@@ -1,13 +1,17 @@
 fun main(){
-    println ("Choose your element")
-    val playerchoice = readLine()
-    println ("You choose" + " " + playerchoice)
-
-
-
-    val options = arrayOf("Rock", "Paper", "Scissors")
+    val options = arrayOf("rock", "paper", "scissors")
     val randomNumber = (0..2).random()                    //https://stackoverflow.com/questions/45685026/how-can-i-get-a-random-number-in-kotlin
                                                           // .. staat voor until
-    println("Computer chooses" + " " + options[randomNumber])
+    val computerChoice = options[randomNumber]
 
+    println ("Choose your element: rock - paper - scissors")
+    val playerChoice = readLine()
+    println ("You choose" + " " + playerChoice)
+
+    println("Computer chooses" + " " + computerChoice)
+
+
+    if (computerChoice == playerChoice) {
+        print("It's a draw")
+    }
 }
