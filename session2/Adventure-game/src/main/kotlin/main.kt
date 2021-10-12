5e3d620511bb7b101a77a47643b59a6a22d14814
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-10-12 10:47:29 +0200

    preparation game
    Intro made for game

--- session2/Adventure-game/src/main/kotlin/main.kt
+++ session2/Adventure-game/src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
 fun main(){
-    println("hello")
+    println("Welcome to the world of disney")
+    println("But there is a problem. Somme of the characters are gone and we don't know where to find them")
+    println("Can you help us to find the missing characters?")
+    println("Enter yes or no")
+
+    val playerInput = readLine()
+    if (playerInput == "yes"){
+        println("Yippie! Pack your bag and let's go!")
+    }
+    if (playerInput == "no"){
+        println("How unfortunate! See you another time")
+    }
+
+
 }
