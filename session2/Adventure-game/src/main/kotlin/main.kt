14c89647558d91f851df4caa0b7bc7d9ae02af46
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-10-12 11:09:55 +0200

    Adding story to challengeOne()
    Make of the first riddle
    Asking if the player is willing to hear riddle
    Adding quotation marks to storyline

--- session2/Adventure-game/src/main/kotlin/main.kt
+++ session2/Adventure-game/src/main/kotlin/main.kt
@@ -1,16 +1,41 @@
 fun main(){
-    println("Welcome to the world of disney")
-    println("But there is a problem. Somme of the characters are gone and we don't know where to find them")
-    println("Can you help us to find the missing characters?")
+    println("'Welcome to the world of disney'")
+    println("'But there is a problem. Somme of the characters are gone and we don't know where to find them'")
+    println("'Can you help us to find the missing characters?'")
     println("Enter yes or no")
 
     val playerInput = readLine()
     if (playerInput == "yes"){
-        println("Yippie! Pack your bag and let's go!")
+        println("'Yippie! Pack your bag and let's go!'")
+        challengeOne()
     }
     if (playerInput == "no"){
-        println("How unfortunate! See you another time")
+        println("'How unfortunate! See you another time'")
     }
+
+
+}
+fun challengeOne(){
+    println("*After walking for a while*")
+    println("'Did you hear that noise? Something is hiding behind that rock'")
+    println("'Would you want to know which character is hiding behind the rock?'")
+    println("Enter yes or no")
+
+    val playerInput2 = readLine()
+    if (playerInput2 != "yes"){
+        println("'Maybe an other time'")
+        println("'Let's keep going'")
+    }
+    if(playerInput2 == "yes"){
+        println("I live in the jungle")
+        println("My father was killed")
+        println("I am to be king of the jungle one day")
+        println("I sing I can’t wait to be king")
+        println("Who am I?")
+        
+    }
+
+
 
 
 }
