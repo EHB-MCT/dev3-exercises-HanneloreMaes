e44635f86450d5b7bca2b98140e826d07d7cee9b
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-10-19 11:11:53 +0200

    Adding greeting
    Adding greeting in main

--- session3/chatroulette-1/src/main/kotlin/main.kt
+++ session3/chatroulette-1/src/main/kotlin/main.kt
@@ -1,8 +1,7 @@
 fun main(){
-    val player1 = readLine()
     val person1 = Person("Player1")
     val person2 = Person("Player2")
 
     val persons = arrayOf(person1.name, person2.name).random()
-    println(persons)
+    println("Hello. My name is $persons")
 }
