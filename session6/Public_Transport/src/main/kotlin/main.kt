ce2f7b514cfd177676dd769c1c1654b762d3f40a
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-11-23 12:56:05 +0100

    Adding second input
    Adding extra input for destination
    Showing train for ride

--- session6/Public_Transport/src/main/kotlin/main.kt
+++ session6/Public_Transport/src/main/kotlin/main.kt
@@ -18,19 +18,23 @@ fun  main(){
                 credentials.databaseName,
         connectionProps)
 
-    println("Choose your city: Brussel, Antwerpen-Centraal or Halle")
-    val search= readLine()
+    println("Choose your Departure City: Brussel, Antwerpen-Centraal or Halle")
+    val start= readLine()
+    println("Choose your Destination: Asse, Genk or Leuven")
+    val destination= readLine()
     val statement = connection.prepareStatement(
-        "SELECT * FROM S6_trains INNER JOIN S6_rides ON S6_trains.id = S6_rides.train_id WHERE city_A=? ORDER BY time_depart LIMIT 1 ")
-    statement.setString(1, search)
+        "SELECT * FROM S6_trains INNER JOIN S6_rides ON S6_trains.id = S6_rides.train_id WHERE city_A=? AND city_B=? ORDER BY time_depart LIMIT 1 ")
+    statement.setString(1, start)
+    statement.setString(2, destination)
     // The result of your query will be a type ResultSet
     // This is a bit similar to collections in Kotlin
     // But since the library is based on Java, we get a type from Java
     val result = statement.executeQuery()
     while(result.next()) {
         val depart = result.getString("city_A")
+        val dest = result.getString("city_B")
         val time = result.getString("time_depart")
-        println(depart)
+        println(depart + " " + "to" + " "  + dest)
         println(time)
     }
 
