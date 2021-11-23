64d680e86b16bf48d9852c400ee3c7dbe0a85d34
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-11-23 12:33:12 +0100

    Main.kt
    Adding login
    Searching on value user
    Showing input of data

--- session6/Public_Transport/src/main/kotlin/main.kt
+++ session6/Public_Transport/src/main/kotlin/main.kt
@@ -1,3 +1,38 @@
+import java.sql.DriverManager
+import java.util.*
+
 fun  main(){
-    println("Helloww");
+    Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance()
+
+    val credentials = Credentials()
+    // Prepare credentials
+    val connectionProps = Properties()
+    connectionProps["user"] = credentials.databaseUser
+    connectionProps["password"] = credentials.databasePassword
+
+    // Create the connection: this will allow us to run queries on it later
+    val connection =  DriverManager.getConnection(
+        "jdbc:" + "mysql" + "://" +
+                credentials.databaseHost +
+                ":" + "3306" + "/" +
+                credentials.databaseName,
+        connectionProps)
+
+    println("Choose your city: Brussel, Antwerpen-Centraal or Halle")
+    val search= readLine()
+    val statement = connection.prepareStatement(
+        "SELECT * FROM S6_trains INNER JOIN S6_rides ON S6_trains.id = S6_rides.train_id WHERE city_A =? ")
+    statement.setString(1, search)
+    // The result of your query will be a type ResultSet
+    // This is a bit similar to collections in Kotlin
+    // But since the library is based on Java, we get a type from Java
+    val result = statement.executeQuery()
+    while(result.next()) {
+        val depart = result.getString("city_A")
+        val time = result.getString("time_depart")
+        println(depart)
+        println(time)
+    }
+
+
 }
