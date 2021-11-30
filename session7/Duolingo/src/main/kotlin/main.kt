88d4f108c1013db317e4c83a9c993b8e38b95332
HanneloreMaes <hannelore.maes@student.ehb.be>
2021-11-30 10:40:42 +0100

    Creation classes
    Creation classes Duolingo.kt and Word.kt
    Adding words of English and German in main()

--- session7/Duolingo/src/main/kotlin/main.kt
+++ session7/Duolingo/src/main/kotlin/main.kt
@@ -1,3 +1,26 @@
 fun main(){
-    println("helllooowwww")
+    val wordsEnglish = arrayOf(
+        Word("Truck", "Vrachtwagen", "English"),
+        Word("Mother", "Moeder", "English"),
+        Word("Friendship", "Vriendschap", "English"),
+        Word("Toy", "Speelgoed", "English"),
+        Word("Voice", "Stem", "English"),
+        Word("Woman", "Vrouw", "English"),
+        Word("Defeat", "Verlies", "English"),
+        Word("Win", "Win", "English"),
+        Word("Mirror", "Spiegel", "English"),
+        Word("Lover", "Geliefde", "English")
+    )
+    val wordsGerman = arrayOf(
+        Word("Nett", "Lief", "Duits"),
+        Word("Klug", "Slim", "Duits"),
+        Word("Schmetterling", "Vlinder", "Duits"),
+        Word("Krakenwagen", "Ziekenwagen", "Duits"),
+        Word("Salat", "Salad", "Duits"),
+        Word("Frau", "Vrouw", "Duits"),
+        Word("Meer", "Zee", "Duits"),
+        Word("Kaffee", "Koffie", "Duits"),
+        Word("Auf Wiedersehen", "Tot ziens", "Duits"),
+        Word("Lederhosen", "Lederen broek", "Duits")
+    )
 }
